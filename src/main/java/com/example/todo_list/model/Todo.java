44a27b4dcc21bccb6bd41b4f57f43f748a1c3977
INT\Victor.Rosario <victor.rosario@solera.com>
2022-10-04 09:34:05 +0200

    Initial

---
+++ src/main/java/com/example/todo_list/model/Todo.java
@@ -0,0 +1,68 @@
+package com.example.todo_list.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+	
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.AUTO)
+	    private long id;
+
+		private String userName;
+
+	    private String description;
+
+	    private Date targetDate;
+
+	    public Todo() {
+	        super();
+	    }
+
+	    public Todo(String user, String desc, Date targetDate, boolean isDone) {
+	        super();
+	        this.setUserName(user);
+	        this.setDescription(desc);
+	        this.setTargetDate(targetDate);
+	    }
+	    
+	    public long getId() {
+			return id;
+		}
+
+		public void setId(long id) {
+			this.id = id;
+		}
+
+		public String getUserName() {
+			return userName;
+		}
+
+		public void setUserName(String userName) {
+			this.userName = userName;
+		}
+
+		public String getDescription() {
+			return description;
+		}
+
+		public void setDescription(String description) {
+			this.description = description;
+		}
+
+		public Date getTargetDate() {
+			return targetDate;
+		}
+
+		public void setTargetDate(Date targetDate) {
+			this.targetDate = targetDate;
+		}
+	
+}
---
+++ src/main/java/com/example/todo_list/service/ITodoService.java
@@ -0,0 +1,23 @@
+package com.example.todo_list.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import com.example.todo_list.model.Todo;
+
+public interface ITodoService {
+
+	List<Todo> getTodosByUser(String user);
+
+	Optional<Todo> getTodoById(long id);
+
+	void updateTodo(Todo todo);
+
+	void addTodo(String name, String desc, Date targetDate, boolean isDone);
+
+	void deleteTodo(long id);
+	
+	void saveTodo(Todo todo);
+
+}
